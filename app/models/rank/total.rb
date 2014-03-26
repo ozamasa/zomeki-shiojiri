7292c28949125491298fcb3a841fa40dcbfdf0d1
Masaki Ozawa <ozamasa123@gmail.com>
2014-03-26 17:23:10 +0900

    Use top level module

--- app/models/rank/total.rb
+++ app/models/rank/total.rb
@@ -19,7 +19,7 @@ class Rank::Total < ActiveRecord::Base
                       tag = ActionController::Base.helpers.image_tag("#{doc.public_uri(without_filename: true)}file_contents/#{image_file.name}")
                     else
                       unless (img_tags = Nokogiri::HTML.parse(doc.body).css('img[src^="file_contents/"]')).empty?
-                        filename = File.basename(img_tags.first.attributes['src'].value) rescue nil
+                        filename = ::File.basename(img_tags.first.attributes['src'].value) rescue nil
                         tag = ActionController::Base.helpers.image_tag("#{doc.public_uri(without_filename: true)}file_contents/#{filename}") unless filename.blank?
                       end
                     end
