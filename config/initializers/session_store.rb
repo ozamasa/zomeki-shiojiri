0b362c657d1a771214759f8e3afc7004dd9ed430
Masaki Ozawa <ozamasa123@gmail.com>
2014-04-15 17:20:51 +0900

    introduce session expire

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ZomekiCMS::Application.config.session_store :cookie_store, key: '_zomeki_cms_session'
+ZomekiCMS::Application.config.session_store :cookie_store, key: '_zomeki_cms_session', expire_after: 15.days
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
