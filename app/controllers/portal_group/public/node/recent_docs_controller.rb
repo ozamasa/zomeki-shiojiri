bcf33d04faec00057c6898ab7ce08375ee860718
Masaki Ozawa <ozamasa123@gmail.com>
2014-07-30 13:56:32 +0900

    fix json feed to portal_group

--- app/controllers/portal_group/public/node/recent_docs_controller.rb
+++ app/controllers/portal_group/public/node/recent_docs_controller.rb
@@ -1,21 +1,32 @@
 # encoding: utf-8
 class PortalGroup::Public::Node::RecentDocsController < Cms::Controller::Public::Base
   include PortalGroup::Controller::Feed
-  
+
   def index
     @content = Page.current_node.content
-    
+
     doc = PortalArticle::Doc.new.public
     doc.agent_filter(request.mobile)
     doc.and :portal_group_id, @content.id
     doc.and :portal_group_state, "visible"
     #doc.visible_in_recent
-    doc.page params[:page], (request.mobile? ? 20 : 50)
-    @docs = doc.find(:all, :order => 'published_at DESC')
+
+    @docs = []
+    if ['rss', 'atom', 'json'].index(params[:format])
+      docs = doc.find(:all, :order => 'published_at DESC')
+      docs.each do |d|
+        @docs << d if d.content.site_id != 1
+      end
+    else
+      doc.page params[:page], (request.mobile? ? 20 : 50)
+      docs = doc.find(:all, :order => 'published_at DESC')
+      @docs = docs
+    end
+
     return true if render_feed(@docs)
-    
+
     return http_error(404) if @docs.current_page > @docs.total_pages
-    
+
     prev = nil
     @items = []
     @docs.each do |doc|
@@ -25,7 +36,7 @@ class PortalGroup::Public::Node::RecentDocsController < Cms::Controller::Public:
         :date => (date != prev ? doc.published_at.strftime('%Y年%-m月%-d日') : nil),
         :doc  => doc
       }
-      prev = date    
+      prev = date
     end
   end
 end
